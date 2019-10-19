d7ea895f5f88ece223aa8a4345418524f58b1de1
zhouhuiMr <1430433546@qq.com>
2019-10-19 16:52:02 +0800

    Update plane.js

--- main/object/plane.js
+++ main/object/plane.js
@@ -11,20 +11,24 @@ window.planeFactory = new Object();
      * 飞机的对象，用此对象进行飞机的创建
      * @since 2019.10.09
      * */
-    var plane = function(scene){
+    let plane = function(scene){
         this.scene = scene;
         this.body =new THREE.Group();
         this.init();
     };
     plane.prototype = {
         init : function(){
-            var wing_front_top_mesh = new wing_front_top();
-            this.body.add(wing_front_top_mesh.build());
+            const wing_front_mesh = new wing_front();
+            this.body.add(wing_front_mesh.build());
 
-            var wing_front_bottom_mesh = new wing_front_bottom();
-            this.body.add(wing_front_bottom_mesh.build());
+            const wing_upholder_mesh = new wing_upholder(
+                wing_front_mesh.width,
+                wing_front_mesh.height,
+                wing_front_mesh.heightDifference
+            );
+            this.body.add(wing_upholder_mesh.build());
 
-            var plane_body_mesh = new plane_body();
+            const plane_body_mesh = new plane_body();
             this.body.add(plane_body_mesh.build());
 
             this.scene.add(this.body);
@@ -32,20 +36,20 @@ window.planeFactory = new Object();
     };
     w.plane = plane;
 
-    var wing_front = function(){
+    let wing_front = function(){
         this.width = 14;
         this.height = 4;
         this.depth = 0.4;
-    };
+        this.heightDifference = 6;//两个机翼之间的高度差
 
-    /**
-     * 飞机机翼的上边机翼
-     * @since2019.10.09
-     * */
-    var wing_front_top = function(){
         this.body = null;
-        this.bodyShape = new THREE.Shape();
+        this.bodyTopShape = new THREE.Shape();
+        this.bodyBottomShape = new THREE.Shape();
+
+        this.bodyTopGeometry = null;
+        this.bodyBottomGeometry = null;
         this.bodyGeometry = null;
+
         this.bodyMaterial = null;
 
         this.materialColor = 0x00ff00;
@@ -53,11 +57,13 @@ window.planeFactory = new Object();
         this.distance_angle = Math.PI / 4;
 
         this.extrudeSettings = null;
+
         this.init();
+
     };
-    wing_front_top.prototype = {
+    wing_front.prototype = {
         init : function(){
-            wing_front.call(this);
+            //挤出的设置
             this.extrudeSettings = {
                 depth: this.depth,
                 bevelEnabled: false,
@@ -67,10 +73,16 @@ window.planeFactory = new Object();
                 bevelThickness: 1,
                 bevelOffset: 0
             };
-
-            this.bodyShape.moveTo(0,0);
-            this.bodyShape.lineTo((this.width - this.distance) / 2, 0);
-            this.bodyShape.absarc(
+            //材质
+            this.bodyMaterial = new THREE.MeshPhysicalMaterial( {
+                color: this.materialColor,
+                side : THREE.FrontSide,
+                wireframe : true,
+            } );
+            //前端的上部机翼
+            this.bodyTopShape.moveTo(0,0);
+            this.bodyTopShape.lineTo((this.width - this.distance) / 2, 0);
+            this.bodyTopShape.absarc(
                 this.width / 2,
                 -1 * this.distance / 2 / Math.tan(this.distance_angle),
                 this.distance / 2 / Math.sin(this.distance_angle) ,
@@ -78,100 +90,169 @@ window.planeFactory = new Object();
                 Math.PI / 2 - this.distance_angle,
                 true
             );
-            this.bodyShape.lineTo(this.width , 0);
-            this.bodyShape.absarc( this.width, this.height / 2 , this.height / 2, -Math.PI / 2, Math.PI / 2, false );
-            this.bodyShape.lineTo(0, this.height);
-            this.bodyShape.absarc( 0, this.height / 2 , this.height / 2, Math.PI / 2, -Math.PI / 2, false );
-            this.bodyGeometry = new THREE.ExtrudeBufferGeometry(this.bodyShape,this.extrudeSettings);
-            this.bodyMaterial = new THREE.MeshPhysicalMaterial( {
-                color: this.materialColor,
-                side : THREE.FrontSide,
-                wireframe : true,
-            } );
-            //设置位置
+            this.bodyTopShape.lineTo(this.width , 0);
+            this.bodyTopShape.absarc( this.width, this.height / 2 , this.height / 2, -Math.PI / 2, Math.PI / 2, false );
+            this.bodyTopShape.lineTo(0, this.height);
+            this.bodyTopShape.absarc( 0, this.height / 2 , this.height / 2, Math.PI / 2, -Math.PI / 2, false );
+            this.bodyTopGeometry = new THREE.ExtrudeBufferGeometry(this.bodyTopShape,this.extrudeSettings);
+
+            this.bodyBottomShape.moveTo(0,0);
+            this.bodyBottomShape.lineTo(this.width , 0);
+            this.bodyBottomShape.absarc( this.width, this.height / 2 , this.height / 2, -Math.PI / 2, Math.PI / 2, false );
+            this.bodyBottomShape.lineTo(0, this.height);
+            this.bodyBottomShape.absarc( 0, this.height / 2 , this.height / 2, Math.PI / 2, -Math.PI / 2, false );
+            this.bodyBottomGeometry = new THREE.ExtrudeBufferGeometry(this.bodyBottomShape,this.extrudeSettings);
+
             this.setSite();
 
-            this.body = new THREE.Mesh( this.bodyGeometry, this.bodyMaterial ) ;
+            const geometries = [this.bodyTopGeometry,this.bodyBottomGeometry];
+            this.bodyGeometry = THREE.BufferGeometryUtils.mergeBufferGeometries(geometries,false);
+
+            this.body = new THREE.Mesh( this.bodyGeometry, this.bodyMaterial );
         },
         setSite : function(){
-            this.bodyGeometry.rotateX(Math.PI / 2);
-            this.bodyGeometry.rotateY(0);
-            this.bodyGeometry.rotateZ(0);
-            this.bodyGeometry.translate(0 ,5.2 ,0);
+            //设置一下顶部机翼的位置
+            this.bodyTopGeometry.rotateX(Math.PI / 2);
+            this.bodyTopGeometry.rotateY(0);
+            this.bodyTopGeometry.rotateZ(0);
+            this.bodyTopGeometry.translate(0 ,this.heightDifference ,0);
+            //设置一下低部机翼的位置
+            this.bodyBottomGeometry.rotateX(Math.PI / 2);
+            this.bodyBottomGeometry.rotateY(0);
+            this.bodyBottomGeometry.rotateZ(0);
+            this.bodyBottomGeometry.translate(0,0,0);
         },
         build : function(){
-            return this.body
+            return this.body;
         }
     };
-    w.wing_front_top = wing_front_top;
+    w.wing_front = wing_front;
 
     /**
-     * 飞机机翼的下边机翼
-     * @since2019.10.09
+     * 飞机机翼的支撑物
+     * @since 2019.10.19
      * */
-    var wing_front_bottom = function(){
+    let wing_upholder = function(wing_width,wing_depth,wing_heightDifference){
         this.body = null;
-        this.bodyShape = new THREE.Shape();
         this.bodyGeometry = null;
         this.bodyMaterial = null;
 
         this.materialColor = 0x00ff00;
 
-        this.extrudeSettings = null;
+        this.wing_width = wing_width;
+        this.wing_depth = wing_depth;
+
+        this.height = wing_heightDifference;
+        this.radiusTop = 0.05;
+        this.radiusBottom = 0.05;
+        this.radialSegments = 4;
+        this.thetaStart = 0;
+        this.thetaLength = Math.PI * 2;
+
         this.init();
     };
-    wing_front_bottom.prototype = {
+    wing_upholder.prototype = {
         init : function(){
-            wing_front.call(this);
-            this.extrudeSettings = {
-                depth: this.depth,
-                bevelEnabled: false,
-                bevelSegments: 2,
-                steps: 1,
-                bevelSize: 0,
-                bevelThickness: 1,
-                bevelOffset: 0
-            };
+            const geometry_1 = new THREE.CylinderBufferGeometry(
+                this.radiusTop,
+                this.radiusBottom,
+                this.height,
+                this.radialSegments,
+                1,
+                false,
+                this.thetaStart,
+                this.thetaLength
+            );
+            const geometry_2 = geometry_1.clone();
+            const geometry_3 = geometry_1.clone();
+            const geometry_4 = geometry_1.clone();
 
-            this.bodyShape.moveTo(0,0);
-            this.bodyShape.lineTo(this.width , 0);
-            this.bodyShape.absarc( this.width, this.height / 2 , this.height / 2, -Math.PI / 2, Math.PI / 2, false );
-            this.bodyShape.lineTo(0, this.height);
-            this.bodyShape.absarc( 0, this.height / 2 , this.height / 2, Math.PI / 2, -Math.PI / 2, false );
-            this.bodyGeometry = new THREE.ExtrudeBufferGeometry(this.bodyShape,this.extrudeSettings);
+            geometry_1.translate(0.5, this.height / 2 - 0.1, 0.5);
+            geometry_2.translate(this.wing_width - 0.5, this.height / 2 - 0.1, 0.5);
+            geometry_3.translate(0.5, this.height / 2 - 0.1, this.wing_depth -0.5);
+            geometry_4.translate(this.wing_width - 0.5, this.height / 2 - 0.1, this.wing_depth -0.5);
+
+            const angle = Math.PI / 3;
+            let upholderExtent = this.height / Math.sin(angle);
+
+            const geometry_5 = new THREE.CylinderBufferGeometry(
+                this.radiusTop,
+                this.radiusBottom,
+                upholderExtent,
+                this.radialSegments,
+                1,
+                false,
+                this.thetaStart,
+                this.thetaLength
+            );
+            const geometry_6 = geometry_5.clone();
+            const geometry_7 = geometry_5.clone();
+            const geometry_8 = geometry_5.clone();
+
+            geometry_5.rotateZ(Math.PI / 2 - angle);
+            geometry_5.translate(upholderExtent * Math.sin(angle) / 2, this.height / 2, 0.5);
+            geometry_6.rotateZ(Math.PI / 2 - angle);
+            geometry_6.translate(upholderExtent * Math.sin(angle) / 2, this.height / 2, this.wing_depth -0.5);
+            geometry_7.rotateZ(-1 * (Math.PI / 2 - angle));
+            geometry_7.translate(this.wing_width - upholderExtent * Math.sin(angle) / 2, this.height / 2, 0.5);
+            geometry_8.rotateZ(-1 * (Math.PI / 2 - angle));
+            geometry_8.translate(this.wing_width - upholderExtent * Math.sin(angle) / 2, this.height / 2, this.wing_depth -0.5);
+
+            let shortUpholderExtent = (this.height - 4) / Math.sin(angle);
+
+            const shortGeometry_1 = new THREE.CylinderBufferGeometry(
+                this.radiusTop,
+                this.radiusBottom,
+                shortUpholderExtent,
+                this.radialSegments,
+                1,
+                false,
+                this.thetaStart,
+                this.thetaLength
+            );
+            const shortGeometry_2 = shortGeometry_1.clone();
+            const shortGeometry_3 = shortGeometry_1.clone();
+            const shortGeometry_4 = shortGeometry_1.clone();
+            shortGeometry_1.rotateZ(Math.PI / 2 - angle);
+            shortGeometry_1.translate(4.8, 4.8, 1.5);
+            shortGeometry_2.rotateZ(Math.PI / 2 - angle);
+            shortGeometry_2.translate(4.8, 4.8, this.wing_depth - 1);
+            shortGeometry_3.rotateZ(-1 * (Math.PI / 2 - angle));
+            shortGeometry_3.translate(9.2, 4.8, 1.5);
+            shortGeometry_4.rotateZ(-1 * (Math.PI / 2 - angle));
+            shortGeometry_4.translate(9.2, 4.8, this.wing_depth - 1);
+
+
+            const geometries = [
+                geometry_1,geometry_2,geometry_3,geometry_4,
+                geometry_5,geometry_6,geometry_7,geometry_8,
+                shortGeometry_1,shortGeometry_2,shortGeometry_3,shortGeometry_4
+            ];
+            this.bodyGeometry = THREE.BufferGeometryUtils.mergeBufferGeometries(geometries,false);
+
             this.bodyMaterial = new THREE.MeshPhysicalMaterial( {
                 color: this.materialColor,
                 side : THREE.FrontSide,
                 wireframe : true,
-            } );
-            //设置位置
-            this.setSite();
-
+            });
             this.body = new THREE.Mesh( this.bodyGeometry, this.bodyMaterial ) ;
         },
-        setSite : function(){
-            this.bodyGeometry.rotateX(Math.PI / 2);
-            this.bodyGeometry.rotateY(0);
-            this.bodyGeometry.rotateZ(0);
-            this.bodyGeometry.translate(0,0,0);
-        },
         build : function(){
-            return this.body
+            return this.body;
         }
     };
-    w.wing_front_bottom = wing_front_top;
-
+    w.wing_upholder = wing_upholder;
     /**
      * 飞机的主体部分
      * @since 2019.10.09
      * */
-    var plane_body = function(){
+    let plane_body = function(){
         this.body = null;
 
         this.radiusTop = 2.5;
         this.radiusBottom = 2.5;
         this.height = 12;
-        this.radialSegments = 12;
+        this.radialSegments = 8;
         this.heightSegments = 10;
         this.thetaStart = 0;
         this.thetaLength = 2 * Math.PI;
@@ -184,11 +265,11 @@ window.planeFactory = new Object();
     };
     plane_body.prototype = {
         init : function(){
-            var height_center = 4.5,
+            const height_center = 4.5,
                 height_front = this.radiusTop,
                 height_back = 9;
             //飞机身体的中间部分
-            var centerBofyOfPlane = new THREE.CylinderBufferGeometry(
+            const centerBofyOfPlane = new THREE.CylinderBufferGeometry(
                 this.radiusTop,
                 this.radiusBottom,
                 height_center,
@@ -199,16 +280,16 @@ window.planeFactory = new Object();
                 this.thetaLength
             );
             //飞机身体的前边部分
-            var frontBodyGeometry = buildFrontBodyOfPlane(height_front,this.radialSegments);
+            const frontBodyGeometry = buildFrontBodyOfPlane(height_front,this.radialSegments);
             frontBodyGeometry.translate(0,height_center / 2 ,0);
 
             //飞机身体的后边部分
-            var backBodyOfPlane = buildBackBodyOfPlane(height_back,this.radiusTop,this.radialSegments);
-            //backBodyOfPlane.translate(0, -1 * (height_back + height_center ) / 2,0);
+            const backBodyOfPlane = buildBackBodyOfPlane(height_back,this.radiusTop,this.radialSegments);
+            backBodyOfPlane.translate(0, -1 * (height_back + height_center ) / 2,0);
 
             //图形合并
-            //var geometries = [frontBodyGeometry,centerBofyOfPlane,backBodyOfPlane];
-            var geometries = [backBodyOfPlane];
+            const geometries = [frontBodyGeometry,centerBofyOfPlane,backBodyOfPlane];
+            //var geometries = [backBodyOfPlane];
             this.bodyGeometry = THREE.BufferGeometryUtils.mergeBufferGeometries(geometries,false);
 
             this.bodyMaterial = new THREE.MeshPhysicalMaterial( {
@@ -216,15 +297,15 @@ window.planeFactory = new Object();
                 side : THREE.FrontSide,
                 wireframe : true,
             } );
-            //this.setSite();
+            this.setSite();
             this.body = new THREE.Mesh( this.bodyGeometry, this.bodyMaterial ) ;
         },
         setSite : function(){
-            var wing = new wing_front();
+            const wing = new wing_front();
             this.bodyGeometry.rotateX(1 * Math.PI / 2);
             this.bodyGeometry.rotateY(0);
             this.bodyGeometry.rotateZ(0);
-            this.bodyGeometry.translate(wing.width / 2,this.radiusTop - wing.depth,2);
+            this.bodyGeometry.translate(wing.width / 2,this.radiusTop - wing.depth,1.5);
         },
         build : function(){
             return this.body;
@@ -237,34 +318,34 @@ window.planeFactory = new Object();
      *@since 2019.10.09
      * */
     function buildFrontBodyOfPlane(height_front,radialSegments){
-        var points = [];
-        var changeAngleRate = Math.PI / 16,//变化的角度数
-            changeAngleTimes = 3;//角度数变化的次数
+        const points = [];
+        const changeAngleRate = Math.PI / 16,//变化的角度数
+               changeAngleTimes = 3;//角度数变化的次数
         //points.push( new THREE.Vector3(0 , 0 ,0));
         points.push( new THREE.Vector3(height_front, 0, 0));
         //0°到45°
-        for ( var i = 1; i <= changeAngleTimes; i ++ ) {
-            var y = height_front * Math.sin(changeAngleRate * i),
+        for ( let i = 1; i <= changeAngleTimes; i ++ ) {
+            let y = height_front * Math.sin(changeAngleRate * i),
                 x = height_front * Math.cos(changeAngleRate * i );
             points.push( new THREE.Vector3(x , y  ,0));
         }
         points.push( new THREE.Vector3( height_front * Math.sin(   changeAngleRate *  changeAngleTimes) - 0.1,height_front * Math.sin(   changeAngleRate *  changeAngleTimes) - 0.2,0));
         points.push( new THREE.Vector3( 0,height_front * Math.sin(   changeAngleRate *  changeAngleTimes) - 0.2,0));
         points.push( new THREE.Vector3( 0 , 0 , 0));
-        var geometry = new THREE.LatheBufferGeometry( points,radialSegments,0,2 * Math.PI);
+        const geometry = new THREE.LatheBufferGeometry( points,radialSegments,0,2 * Math.PI);
         return geometry;
-    };
+    }
 
     /**
      *飞机的本体的后端部分
      *@since 2019.10.13
      * */
     function buildBackBodyOfPlane(height_back,radiusTop,radialSegments){
-        var heightSegments = 1,//高度的分段数
-            radiusBottom = 0.2;//底部圆的半径
-        var moveDistince = (radiusTop - radiusBottom) / 2,
-            max_y = height_back / 2 - 0.01;
-        var backBodyOfPlane = new THREE.CylinderBufferGeometry(
+        const heightSegments = 1,//高度的分段数
+               radiusBottom = 0.2;//底部圆的半径
+        let moveDistince = (radiusTop - radiusBottom) / 2,
+               max_y = height_back / 2 - 0.01;
+        const backBodyOfPlane = new THREE.CylinderBufferGeometry(
             radiusTop,
             radiusBottom,
             height_back,
@@ -274,13 +355,13 @@ window.planeFactory = new Object();
             0,
             Math.PI * 2
         );
-        var vertex = new THREE.Vector3();
-        var backBodyOfPlane_Position = backBodyOfPlane.attributes.position;
-        var positionArray = new Array();
+        const vertex = new THREE.Vector3();
+        const backBodyOfPlane_Position = backBodyOfPlane.attributes.position;
+        const positionArray = new Array();
         for(i = 0;i<backBodyOfPlane_Position.count;i++){
             vertex.fromBufferAttribute( backBodyOfPlane_Position, i );
             if(vertex.y < 0){
-                vertex.z -= radiusTop / 2;
+                vertex.z -= radiusTop / 2 - 0.4;
             }
             positionArray.push(vertex.x);
             positionArray.push(vertex.y);
